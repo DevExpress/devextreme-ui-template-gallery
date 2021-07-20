fb56c71ce15a2e847a394b27e3ba56d7834a842e
babich-a <alexey.babych@gmail.com>
2021-07-20 14:39:11 +0300

    Add message about size

---
+++ utils/resize-listener.js
@@ -0,0 +1,53 @@
+class ResizeListener {
+    constructor() {
+        this.resizeObserver = new ResizeObserver(entries => {
+            for(let entry of entries) {
+                if(entry.contentRect && entry.contentRect.height) {
+                    window.parent.postMessage({
+                        type: 'frame-height',
+                        height: entry.contentRect.height
+                    }, '*');
+                }
+            }
+        });
+
+        this.mutationObserver = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if(!mutation.addedNodes) return;
+
+                if(mutation.target && this.isValidNode(mutation.target)) {
+                    this.subscribe(mutation.target);
+                    return;
+                }
+
+                mutation.addedNodes.forEach(node => {
+                    if(this.isValidNode(node)) {
+                        this.subscribe(node);
+                    }
+                });
+            })
+        });
+    }
+
+    isValidNode(node) {
+        return node.classList && node.classList.contains('content');
+    }
+
+    subscribe(node) {
+        this.mutationObserver.disconnect();
+        node.style.alignSelf = 'flex-start';
+        this.resizeObserver.observe(node);
+    }
+
+    listen() {
+        this.mutationObserver.observe(document.body, {
+            childList: true,
+            subtree: true,
+            attributes: false,
+            characterData: false
+        });
+    }
+}
+
+const listener = new ResizeListener();
+listener.listen();
