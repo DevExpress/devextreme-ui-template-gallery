5c0dce11d0f622270a65e84ad8e76e3a47c246be
Alexey Babich <alexey.babych@gmail.com>
2021-07-20 14:54:25 +0300

    Add message about size (#3)

--- utils/make-embedded-mode.js
+++ utils/make-embedded-mode.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync, linkSync, unlinkSync, existsSync } = require('fs');
 const { join } = require('path');
 const { cwd } = require('process');
+const packages = require('./packages');
 
 const rootPath = join(cwd(), 'packages');
 
@@ -34,10 +35,34 @@ const fixVueProject = () => {
     performFileReplacement(targetFileName, /AppFooter/s, '');
 };
 
+const addResizeListener = () => {
+    const listenerFileName = 'resize-listener.js'
+    const listenerPath = join(rootPath, '..', 'utils', listenerFileName);
+
+    packages.forEach(pkg => {
+        const srcPath = join(rootPath, pkg, 'src');
+        const applicationListenerPath = join(srcPath, listenerFileName);
+        if(existsSync(applicationListenerPath)) unlinkSync(applicationListenerPath);
+        linkSync(listenerPath, applicationListenerPath);
+
+        let targetFile = 'main.ts';
+        if(pkg === 'react') {
+            targetFile = 'App.js';
+        } else if(pkg === 'vue') {
+            targetFile = 'main.js';
+        }
+
+        const targetFilePath = join(srcPath, targetFile);
+        const targetFileContent = readFileSync(targetFilePath);
+        writeFileSync(targetFilePath, `import './resize-listener';\n${targetFileContent}`, 'utf8');
+    });
+};
+
 const performReplacements = () => {
     fixAngularProject();
     fixReactProject();
     fixVueProject();
+    addResizeListener();
 };
 
 performReplacements();
