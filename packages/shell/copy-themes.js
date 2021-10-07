a960f3b1129dafacf3785fe32e7b39678bbeec0f
babich-a <alexey.babych@gmail.com>
2021-10-07 10:59:36 +0300

    read themes in shell from themes.json

---
+++ packages/shell/copy-themes.js
@@ -0,0 +1,10 @@
+const { copyFile } = require('fs');
+const { join } = require('path');
+
+copyFile(
+  join(__dirname, '..', 'metadata', 'themes.json'),
+  join(__dirname, 'src', 'app', 'themes.json'),
+  (err) => {
+    if (err) throw err;
+    console.log('themes.json copied');
+});
