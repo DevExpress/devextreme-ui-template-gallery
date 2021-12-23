9f4c1a893d472fa3b2bf2076830ba4ed0e0380e6
babich-a <alexey.babych@gmail.com>
2021-12-23 16:28:26 +0300

    Form - right part

--- packages/angular/src/app/pages/crm-contact-form/crm-contact-form.component.ts
+++ packages/angular/src/app/pages/crm-contact-form/crm-contact-form.component.ts
@@ -10,6 +10,12 @@ export class CrmContactFormComponent implements OnInit, AfterViewInit {
 
   constructor(private screen: ScreenService) {
     getContact(10).then((data) => {
+      // TODO need the following additional fields
+      // - firstName
+      // - secondName
+      // - city
+      // - state
+      // - zipcode
       this.viewData = data;
       this.load = false;
     });
@@ -18,6 +24,10 @@ export class CrmContactFormComponent implements OnInit, AfterViewInit {
 
   viewData: any;
   load = true;
+
+  formatPhone(number: string | number): string {
+    return String(number).replace(/(\d{3})(\d{3})(\d{4})/,"+1($1)$2-$3");
+  }
 
   ngOnInit(): void {
     
