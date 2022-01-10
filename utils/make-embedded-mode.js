807796ec44e22bfa4aac6c07d8d5ff81b15cf78b
babich-a <alexey.babych@gmail.com>
2022-01-10 16:10:58 +0300

    Change embedded mode applicaton (just class)

--- utils/make-embedded-mode.js
+++ utils/make-embedded-mode.js
@@ -7,33 +7,11 @@ const packages = require('./packages');
 
 const rootPath = join(cwd(), 'packages');
 
-const performFileReplacement = (fileName, searchValue, replaceValue) => {
-    const content = readFileSync(fileName).toString();
-    writeFileSync(fileName, content.replace(searchValue, replaceValue), 'utf8');
+const performIndexReplacement = (indexFileLocation) => {
+    const indexFilePath = join(indexFileLocation, 'index.html');
+    const content = readFileSync(indexFilePath).toString();
+    writeFileSync(indexFilePath, content.replace(/class="dx-viewport"/s, 'class="dx-viewport embedded"'), 'utf8');
 }
-
-const fixAngularProject = () => {
-    const targetFileName = join(rootPath, 'angular', 'src', 'app', 'app.component.html');
-    performFileReplacement(targetFileName, /<app-footer>.*<\/app-footer>/s, '');
-    performFileReplacement(targetFileName, /<app-side-nav-outer-toolbar.*?>/s, '<div class="content">');
-    performFileReplacement(targetFileName, /<\/app-side-nav-outer-toolbar>/s, '</div>');
-};
-
-const fixReactProject = () => {
-    const targetFileName = join(rootPath, 'react', 'src', 'Content.js');
-    performFileReplacement(targetFileName, /<Footer>.*<\/Footer>/s, '');
-    performFileReplacement(targetFileName, /<SideNavBarLayout.*?>/s, '<div class="content">');
-    performFileReplacement(targetFileName, /<\/SideNavBarLayout>/s, '</div>');
-};
-
-const fixVueProject = () => {
-    const targetFileName = join(rootPath, 'vue', 'src', 'App.vue');
-    performFileReplacement(targetFileName, /<template #footer>.*?<\/template>/s, '');
-    performFileReplacement(targetFileName, /<component.*?>/s, '');
-    performFileReplacement(targetFileName, /<\/component>/s, '');
-    performFileReplacement(targetFileName, /import AppFooter from ".\/components\/app-footer";/s, '');
-    performFileReplacement(targetFileName, /AppFooter/s, '');
-};
 
 const addResizeListener = () => {
     const listenerFileName = 'resize-listener.js'
@@ -59,9 +37,10 @@ const addResizeListener = () => {
 };
 
 const performReplacements = () => {
-    fixAngularProject();
-    fixReactProject();
-    fixVueProject();
+    performIndexReplacement(join(rootPath, 'angular', 'src'));
+    performIndexReplacement(join(rootPath, 'react', 'public'));
+    performIndexReplacement(join(rootPath, 'vue', 'public'));
+
     addResizeListener();
 };
 
