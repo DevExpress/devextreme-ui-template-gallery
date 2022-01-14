1388a362f2a1191d554927a6c18e57eb6fe960bd
babich-a <alexey.babych@gmail.com>
2022-01-14 10:54:28 +0300

    Add more test and etalons for form view

--- packages/testing/tests/crm-contact-form.test.js
+++ packages/testing/tests/crm-contact-form.test.js
@@ -18,6 +18,25 @@ packages.forEach(pkg => {
             await setEmbeddedMode(embedded);
             await t.expect(Selector('.toolbar-header').withText('Sammy Hill').count).eql(1);
             await takeScreenshot(`crm-contact-form-${pkg.name}-embed=${embedded}-1`, 'body');
+            await t.click(Selector('.dx-button[aria-label=Edit]'));
+            await takeScreenshot(`crm-contact-form-${pkg.name}-embed=${embedded}-2`, 'body');
+
+            await t
+                .expect(compareResults.isValid())
+                .ok(compareResults.errorMessages());
+        });
+
+        test(`Crm contact form tabpanel (${pkg.name}, embed=${embedded})`, async t => {
+            const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+            await t.navigateTo(`http://localhost:${pkg.port}/#/crm-contact-form`);
+            await setEmbeddedMode(embedded);
+            await t.expect(Selector('.toolbar-header').withText('Sammy Hill').count).eql(1);
+
+            for(let i = 0; i < 5; i++) {
+                await t.click(Selector('.dx-tab').nth(i));
+                await takeScreenshot(`crm-contact-form-tab-${i}-${pkg.name}-embed=${embedded}`, 'body');
+            }
 
             await t
                 .expect(compareResults.isValid())
