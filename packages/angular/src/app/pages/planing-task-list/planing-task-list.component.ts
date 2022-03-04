88d39ab4e80c42dfae4f309c07f341e3473475db
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2022-03-04 13:54:01 +0300

    Support display DataGrid tasks (#32)

--- packages/angular/src/app/pages/planing-task-list/planing-task-list.component.ts
+++ packages/angular/src/app/pages/planing-task-list/planing-task-list.component.ts
@@ -1,20 +1,44 @@
-import { Component, ViewChild, OnInit, AfterViewInit, NgModule } from '@angular/core';
+import { Component, OnInit, NgModule, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   DxButtonModule,
-  DxCheckBoxModule,
   DxDataGridModule,
   DxTabsModule,
-  DxFormModule,
-  DxLoadPanelModule,
-  DxSelectBoxModule,
   DxDropDownButtonModule,
-  DxTabPanelModule,
-  DxTextAreaModule,
   DxTextBoxModule,
-  DxTileViewModule,
   DxToolbarModule,
+  DxProgressBarModule,
+
+  DxTabsComponent,
+  DxDataGridComponent
 } from 'devextreme-angular';
+import ArrayStore from 'devextreme/data/array_store';
+import DataSource from 'devextreme/data/data_source';
+
+enum Priority {
+  Low = "Low",
+  Normal = "Normal",
+  Hight = "Hight",
+}
+
+enum Status {
+  Open = "Open",
+  Deferred = "Deferred",
+  Completed = "Completed",
+  InProgress = "In Progress",
+}
+
+type TaskType = {
+  id: number
+  name: string,
+  description: string,
+  company: string,
+  priority: Priority,
+  startDate: Date,
+  dueDate: Date,
+  owner: string,
+  status: Status
+}
 
 @Component({
   // selector: 'app-planing-task-list',
@@ -22,11 +46,127 @@ import {
   styleUrls: ['./planing-task-list.component.scss']
 })
 export class PlaningTaskListComponent implements OnInit {
+  @ViewChild('dataGridTasks', { static: false }) dataGrid: DxDataGridComponent;
+
+  priorityList = ['Low', 'Normal', 'Hight'];
+  statusList = ['Open', 'Deferred', 'Completed', 'In Progress'];
+
+  data: Array<TaskType> = [
+    {
+      id: 1,
+      name: 'Task Name',
+      description: 'Descr',
+      company: 'DevEx',
+      priority: Priority.Low,
+      startDate: new Date(),
+      dueDate: new Date,
+      owner: 'First Last',
+      status: Status.Open,
+    },
+    {
+      id: 2,
+      name: 'Task Name',
+      description: 'Descr',
+      company: 'DevEx',
+      priority: Priority.Normal,
+      startDate: new Date(),
+      dueDate: new Date,
+      owner: 'First Last',
+      status: Status.Deferred,
+    },
+    {
+      id: 3,
+      name: 'Task Name',
+      description: 'Descr',
+      company: 'DevEx',
+      priority: Priority.Hight,
+      startDate: new Date(),
+      dueDate: new Date,
+      owner: 'First Last',
+      status: Status.Completed,
+    },
+    {
+      id: 4,
+      name: 'Task Name',
+      description: 'Descr',
+      company: 'DevEx',
+      priority: Priority.Low,
+      startDate: new Date(),
+      dueDate: new Date,
+      owner: 'First Last',
+      status: Status.InProgress,
+    }
+  ];
+  dataSource: DataSource;
+
+  tabPanelItems: DxTabsComponent['items'] = [
+    {
+      text: 'List'
+    },
+    {
+      text: 'Kanban Board'
+    },
+    {
+      text: 'Gantt'
+    }
+  ];
+
+  displayTaskComponent: string = this.tabPanelItems[0].text;
+
+  priorityValidationPattern = new RegExp(`${Priority.Low}|${Priority.Normal}|${Priority.Hight}`);
 
   constructor() {
+    this.dataSource = new DataSource({
+      key: 'id',
+      store: new ArrayStore({
+        data: this.data,
+      })
+    });
   }
 
-  load = true;
+  spaceToUnderscore = (value) => value.replace(/\ /g, '-');
+
+  customizeHyphetText = (cellInfo) => cellInfo.value ?? '-';
+
+  customizeDateText = (cellInfo) => {
+    if(!cellInfo.value) return this.customizeHyphetText(cellInfo);
+
+    const date: Date = new Date(cellInfo.value);
+    return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+  }
+
+  onRowPreparedGrid = (e) => {
+    const { rowType, data, rowElement }:
+      { rowType: string, data: TaskType, rowElement: HTMLElement  } = e;
+
+    if(rowType === 'header') return;
+
+    if(data.status === Status.Completed) {
+      rowElement.style.background = '#F5F5F5';
+    }
+  }
+
+  tabValueChange = (e) => {
+    const { itemData } = e;
+    this.displayTaskComponent = itemData.text;
+  }
+
+  refreshGrid = () => {
+    this.dataGrid.instance.refresh();
+  }
+
+  dueDateValid = (e) => {
+    const { startDate, dueDate, brokenRules } = e.newData;
+    if(startDate === undefined || dueDate === undefined) {
+      e.errorText = `Need set 'Start Date' and 'Due Date'`;
+      e.isValid = false;
+    } else if(dueDate <= startDate) {
+      e.errorText = `'Start Date' must be greater 'Due Date'`;
+      e.isValid = false;
+    } else if (brokenRules.length !== 0) {
+      e.errorText = 'All fields must be filled'
+    }
+  }
 
   ngOnInit(): void {
   }
@@ -35,18 +175,12 @@ export class PlaningTaskListComponent implements OnInit {
 @NgModule({
   imports: [
     DxButtonModule,
-    DxCheckBoxModule,
     DxDataGridModule,
     DxTabsModule,
-    DxFormModule,
-    DxLoadPanelModule,
-    DxSelectBoxModule,
     DxDropDownButtonModule,
-    DxTabPanelModule,
-    DxTextAreaModule,
     DxTextBoxModule,
-    DxTileViewModule,
     DxToolbarModule,
+    DxProgressBarModule,
 
     CommonModule
   ],
---
+++ packages/testing/tests/planning-task-list.test.js
@@ -0,0 +1,35 @@
+import { Selector, ClientFunction  } from 'testcafe';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { packages, screenModes } from '../config.js';
+
+fixture`Planning List`;
+
+const setEmbeddedMode = ClientFunction((embed) => {
+    if(!embed) return;
+    window.document.getElementsByTagName('body')[0].classList.add('embedded');
+});
+
+packages.forEach(pkg => {
+    [false, true].forEach(embedded => {
+        screenModes.forEach(screenMode => {
+            test(`Crm contact list (${pkg.name}, embed=${embedded}, ${screenMode[0]})`, async t => {
+                const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+                await t.resizeWindow(...screenMode);
+
+                await t.navigateTo(`http://localhost:${pkg.port}/#/planning-task-list`);
+                await setEmbeddedMode(embedded);
+                await t.expect(Selector('body.dx-device-generic').count).eql(1);
+                await takeScreenshot(`planning-task-list-${pkg.name}-embed=${embedded}-1-${screenMode[0]}`, 'body');
+                await t.click(Selector('.content .dx-toolbar .dx-tabs .dx-item').nth(1))
+                await takeScreenshot(`planning-task-list-${pkg.name}-embed=${embedded}-2-${screenMode[0]}`, 'body');
+                await t.click(Selector('.content .dx-toolbar .dx-tabs .dx-item').nth(2))
+                await takeScreenshot(`planning-task-list-${pkg.name}-embed=${embedded}-3-${screenMode[0]}`, 'body');
+
+                await t
+                    .expect(compareResults.isValid())
+                    .ok(compareResults.errorMessages());
+            });
+        });
+    })
+});
