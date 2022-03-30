26b4ae3622a2c29551b1ccf5ad7a10c59f6b5e64
OlegKipchatov <olegkipchatov@Olegs-MacBook-Pro.local>
2022-03-30 15:02:26 +0400

    Add tests

--- packages/testing/tests/planning-task-details.test.js
+++ packages/testing/tests/planning-task-details.test.js
@@ -2,7 +2,7 @@ import { Selector, ClientFunction  } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { packages, screenModes } from '../config.js';
 
-fixture`Planning List`;
+fixture`Planning Details`;
 
 const setEmbeddedMode = ClientFunction((embed) => {
     if(!embed) return;
@@ -12,19 +12,28 @@ const setEmbeddedMode = ClientFunction((embed) => {
 packages.forEach(pkg => {
     [false, true].forEach(embedded => {
         screenModes.forEach(screenMode => {
-            test(`Planning task list (${pkg.name}, embed=${embedded}, ${screenMode[0]})`, async t => {
+            test(`Planning task details (${pkg.name}, embed=${embedded}, ${screenMode[0]})`, async t => {
                 const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
                 await t.resizeWindow(...screenMode);
 
-                await t.navigateTo(`http://localhost:${pkg.port}/#/planning-task-list`);
+                await t.navigateTo(`http://localhost:${pkg.port}/#/planning-task-details`);
                 await setEmbeddedMode(embedded);
                 await t.expect(Selector('body.dx-device-generic').count).eql(1);
-                await takeScreenshot(`planning-task-list-${pkg.name}-embed=${embedded}-1-${screenMode[0]}`, 'body');
-                await t.click(Selector('.content .dx-toolbar .dx-tabs .dx-item').nth(1))
-                await takeScreenshot(`planning-task-list-${pkg.name}-embed=${embedded}-2-${screenMode[0]}`, 'body');
-                await t.click(Selector('.content .dx-toolbar .dx-tabs .dx-item').nth(2))
-                await takeScreenshot(`planning-task-list-${pkg.name}-embed=${embedded}-3-${screenMode[0]}`, 'body');
+
+                await takeScreenshot(
+                    `planning-task-details-${pkg.name}-embed=${embedded}-${screenMode[0]}`,
+                    'body');
+
+                await t.click(Selector('.dx-tab').nth(1));
+                await takeScreenshot(
+                    `planning-task-details-${pkg.name}-notes-embed=${embedded}-${screenMode[0]}`,
+                    '.dx-tabpanel-container');
+
+                await t.click(Selector('.dx-tab').nth(2));
+                await takeScreenshot(
+                    `planning-task-details-${pkg.name}-messages-embed=${embedded}-${screenMode[0]}`,
+                    '.dx-tabpanel-container');
 
                 await t
                     .expect(compareResults.isValid())
