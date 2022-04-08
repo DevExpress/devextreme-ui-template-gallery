cf1cff5e8e15d56bd897814428fde924b4921340
groshenkovamarina <marina.groshenkova@devexpress.com>
2022-04-08 19:13:16 +0300

    Fix task details data

--- packages/angular/src/app/pages/planing-task-details/planing-task-details.component.ts
+++ packages/angular/src/app/pages/planing-task-details/planing-task-details.component.ts
@@ -6,6 +6,7 @@ import {
   DxToolbarModule,
   DxTabPanelModule,
   DxTextAreaModule,
+  DxLoadPanelModule,
 } from 'devextreme-angular';
 import {
   ActivitiesModule,
@@ -15,7 +16,8 @@ import {
   TaskProirityModule,
   TaskStatusModule,
 } from 'src/app/shared/components';
-import { TaskType } from 'src/app/shared/types/TaskType';
+import { task } from 'src/app/shared/types/task';
+import { getTask } from 'dx-rwa-data';
 
 @Component({
   // selector: 'app-planing-task-details',
@@ -23,53 +25,31 @@ import { TaskType } from 'src/app/shared/types/TaskType';
   styleUrls: ['./planing-task-details.component.scss']
 })
 export class PlaningTaskDetailsComponent implements OnInit {
-  task: TaskType = {
-    id: 0,
-    name: 'Call to clarify customer requirements',
-    description: 'It\'s a good idea for an employer to maintain a personnel file for each employee.',
-    company: 'Super Mart of the West',
-    priority: 'Normal',
-    status: 'Open',
-    startDate: new Date('11/17/2021'),
-    dueDate: null,
-    owner: 'Brett Johnson'
-  };
+  task: any;
 
-  activities = [
-    {
-      name: 'Assigned to Brett Johnson',
-      date: '09/26/2021',
-      manager: 'Brett Johnson',
-    },
-    {
-      name: 'Open Task',
-      date: '09/24/2021',
-      manager: 'Brett Johnson',
-    }
-  ];
 
-  notes: Array<any> | Promise<Array<any>> = [
-    {
-      date: new Date('09/29/2021'),
-      manager: 'Brett Johnson',
-      text: 'The first call wasn\'t productive. Their representative didn\'t have much time to talk. I tried to discover if they need our toutch screens, but we decided to meet in person, so I could describe our products in greater details.'
-    }
-  ];
+  taskId = 1;
+  load = true;
 
-  messages: Array<any> | Promise<Array<any>> = [
-    {
-      subject: 'Great news about your equipment upgrade',
-      date: new Date('09/29/2021'),
-      manager: 'Brett Johnson',
-      text: 'Hello {username}, It\'s John. Yesterday we talked about audio equipment upgrade in your company. I have great news for you. We\'ve discussed your requirements and I can offer a 15% discount for your if you plan to order for more than $50000. If you\'d like to get a 3 year contract, then we are ready to offer a 20% discount for you.'
-    }
-  ];
+  constructor() { 
+    this.refresh = this.refresh.bind(this);
 
-  constructor() { }
+    getTask(this.taskId).then((data) => {
+      this.task = data;
+      this.load = false;
+    });
+  }
+
+  refresh() {
+    this.load = true;
+    getTask(this.taskId).then((data) => {
+      this.task = data;
+      this.load = false;
+    });
+  }
 
   ngOnInit(): void {
-    this.notes = Promise.resolve(this.notes);
-    this.messages = Promise.resolve(this.messages);
+
   }
 }
 
@@ -80,6 +60,7 @@ export class PlaningTaskDetailsComponent implements OnInit {
     DxToolbarModule,
     DxTabPanelModule,
     DxTextAreaModule,
+    DxLoadPanelModule,
 
     ActivitiesModule,
     NotesModule,
