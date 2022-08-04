cf696cd518656073aac868238b465ad8b19cbe78
Shpileva Yuliya <krijovnick@gmail.com>
2022-08-04 12:23:25 +0400

    fix reordering cards in kanban

--- packages/react/src/components/plannin-kanban/PlanningKanban.tsx
+++ packages/react/src/components/plannin-kanban/PlanningKanban.tsx
@@ -9,53 +9,50 @@ const initialStatuses = ['Open', 'In Progress', 'Deferred', 'Completed'];
 const reorder = (items, item, fromIndex, toIndex) => {
   let result = items;
   if (fromIndex >= 0) {
-      result = [...items.slice(0, fromIndex), ...items.slice(fromIndex + 1)];
+      result = [...result.slice(0, fromIndex), ...result.slice(fromIndex + 1)];
   }
 
   if (toIndex >= 0) {
-      result = [...items.slice(0, toIndex), item, ...items.slice(toIndex)];
+      result = [...result.slice(0, toIndex), item, ...result.slice(toIndex)];
   }
 
   return result;
 };
-
+const updateTask = (list, itemData, fromIndex, toIndex) => {
+  return reorder(list, itemData, fromIndex, toIndex);
+};
 const PlanningKanban = ({ dataSource }) => {
-    const initialLists = [] as any;
-
+  const [loading, setLoading] = useState(true);
+  const [lists, setLists] = useState<any>([]);
+  const [statuses, setStatuses] = useState(initialStatuses);
     useEffect(() => {
         if(dataSource.length !== 0) {
           setLoading(false);
         }
-      }, [dataSource]);  
-
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState(dataSource);
-    initialStatuses.forEach((status) => {
-        initialLists.push(data.filter((task) => task.status === status));
-    });
-    const [lists, setLists] = useState(initialLists);
-    const [statuses, setStatuses] = useState(initialStatuses);
+        const initialLists: any[] = [];
+        initialStatuses.forEach((status) => {
+          initialLists.push(dataSource.filter((task) => task.status === status));
+      });
+      setLists(initialLists);
+      }, [dataSource]);
     const onListReorder = useCallback((e) => {
-        setLists(reorder(lists, lists[e.fromIndex], e.fromIndex, e.toIndex));
-        setStatuses(reorder(statuses, statuses[e.fromIndex], e.fromIndex, e.toIndex));
+      const { fromIndex, toIndex } = e;
+      setLists(reorder(lists, lists[fromIndex], fromIndex, toIndex));
+      setStatuses(reorder(statuses, statuses[e.fromIndex], e.fromIndex, e.toIndex));
     }, [lists, statuses]);
 
     const onTaskDragStart = useCallback((e) => {
         e.itemData = lists[e.fromData][e.fromIndex];
     }, [lists]);
 
-    const updateTask = useCallback((listIndex, itemData, fromIndex, toIndex) => {
-      const updatedLists = lists.slice();
-
-      updatedLists[listIndex] = reorder(updatedLists[listIndex], itemData, fromIndex, toIndex);
-
-      setLists(updatedLists);
-  }, [lists]);
-
     const onTaskDrop = useCallback((e) => {
-        updateTask(e.fromData, e.itemData, e.fromIndex, -1);
-        updateTask(e.toData, e.itemData, -1, e.toIndex);
-    }, [updateTask]);
+        const updatedList = lists.slice();
+        e.itemData.status = statuses[e.toData];
+        updatedList[e.fromData] = updateTask(updatedList[e.fromData], e.itemData, e.fromIndex, -1);
+        updatedList[e.toData] = updateTask(updatedList[e.toData], e.itemData, -1, e.toIndex);
+
+        setLists(updatedList);
+    }, [lists, statuses]);
 
     return (
       loading ? <LoadPanel container=".content" visible position={{ of: '.content' }} /> :
