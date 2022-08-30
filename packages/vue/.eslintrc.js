9a33966293ade78da7380386ee7e1754d2518a39
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2022-08-30 22:17:37 +0400

    RWA Vue TS add lint

---
+++ packages/vue/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    '@vue/eslint-config-airbnb',
+    '@vue/typescript/recommended',
+    'plugin:vue/vue3-essential',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-underscore-dangle': 'off',
+    'vuejs-accessibility/click-events-have-key-events': 'off',
+    'class-methods-use-this': 'off',
+    'vue/require-valid-default-prop': 'off',
+
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
