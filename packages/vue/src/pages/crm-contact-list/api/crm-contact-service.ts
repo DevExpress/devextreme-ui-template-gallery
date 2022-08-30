cb47eb6a7f7d91b1e5c30976e7a97b2447442971
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2022-08-30 21:59:19 +0400

    RWA Vue TS add lint

--- packages/vue/src/pages/crm-contact-list/api/crm-contact-service.ts
+++ packages/vue/src/pages/crm-contact-list/api/crm-contact-service.ts
@@ -1,6 +1,6 @@
+import { AxiosResponse } from 'axios';
 import http from '@/api/http';
 import { Contact } from '@/types/contact';
-import { AxiosResponse } from 'axios';
 
 class CrmContactService {
   getAll() {
