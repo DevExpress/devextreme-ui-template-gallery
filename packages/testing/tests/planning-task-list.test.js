7484cf51ac9f271d8802a51610179437154a4585
Shpileva Yuliya <krijovnick@gmail.com>
2022-09-06 11:27:05 +0200

    test

--- packages/testing/tests/planning-task-list.test.js
+++ packages/testing/tests/planning-task-list.test.js
@@ -3,32 +3,29 @@
 import { Selector } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { toogleEmbeddedClass } from './utils';
-import { screenModes, timeoutSecond } from '../config.js';
+import { screenModes, timeoutSecond, fakeScreenSize } from '../config.js';
 
 const project = process.env.project;
 const BASE_URL = `http://localhost:${process.env.port}`;
 
 fixture`Planning List`;
 
-[true].forEach((embedded) => {
+[false, true].forEach((embedded) => {
   screenModes.forEach((screenMode) => {
     test(`Planning task list (${project}, embed=${embedded}, ${screenMode[0]})`, async (t) => {
       const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-      // await t.resizeWindow(...screenMode);
-
       await t.navigateTo(`${BASE_URL}/#/planning-task-list`);
 
       await toogleEmbeddedClass(embedded);
+      if (embedded) {
+        await t.resizeWindow(...fakeScreenSize);
+      }
+      await t.resizeWindow(...screenMode);
 
       await t.wait(timeoutSecond);
 
-      // await t.expect(Selector('body.dx-device-generic').count).eql(1);
       await takeScreenshot(`planning-task-grid-embed=${embedded}-project=${project}-${screenMode[0]}`, 'body');
-      // await t.click(Selector('.content .dx-toolbar .dx-tabs .dx-item').nth(1));
-      // await takeScreenshot(`planning-task-kanban-embed=${embedded}-${screenMode[0]}`, 'body');
-      // await t.click(Selector('.content .dx-toolbar .dx-tabs .dx-item').nth(2));
-      // await takeScreenshot(`planning-task-gantt-embed=${embedded}-${screenMode[0]}`, 'body');
 
       await t
         .expect(compareResults.isValid())
