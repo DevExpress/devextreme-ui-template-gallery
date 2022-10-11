6e42ecab7b5e11913820e10560731119b16ae990
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2022-10-11 16:01:26 +0400

    Angular. Refactor customization and styles of  view-edit-item

--- packages/angular/src/app/shared/components/edit-view-item/edit-view-item.component.ts
+++ packages/angular/src/app/shared/components/edit-view-item/edit-view-item.component.ts
@@ -1,46 +1,46 @@
 import {
-  Component, Input, NgModule, Output, EventEmitter,
+  Component, Directive, Input, NgModule, Output, EventEmitter, ContentChild,
 } from '@angular/core';
 import {DxButtonModule, DxTextBoxModule, DxValidatorModule} from 'devextreme-angular';
 import { CommonModule } from '@angular/common';
+
+@Directive({selector: '[view-content]'})
+export class ViewContentDirective {}
+
+@Directive({selector: '[editor-content]'})
+export class EditorContentDirective {}
 
 @Component({
   selector: 'edit-view-item',
   template: `
     <ng-container *ngIf="!isEditing; else editing">
       <i *ngIf="icon" class="dx-icon dx-icon-{{ icon }}"></i>
-      <div class="edit-view-item-wrapper"
-           [class.with-label]="!icon">
+      <div class="view-content-wrapper" [class.with-label]="!icon">
         <label *ngIf="!icon" class="dx-texteditor-label">{{ label }}</label>
-        <div #refViewContent class="content-ng-wrapper">
-          <ng-content select="[view-content]"></ng-content>
-        </div>
-        <ng-container *ngIf="refViewContent.children.length === 0">
-          <span class="edit-view-item-value">{{ renderedValue ? renderedValue : value }}</span>
-        </ng-container>
+        <ng-content select="[view-content]"></ng-content>
+        <span *ngIf="!viewContent"
+              class="view-content-value">
+          {{ renderedValue ? renderedValue : value }}
+        </span>
       </div>
     </ng-container>
 
     <ng-template #editing>
-      <div class="editorContentWrapper">
-        <div #refEditorContent class="content-ng-wrapper">
-          <ng-content select="[editor-content]"></ng-content>
-        </div>
-        <ng-container *ngIf="refEditorContent.children.length === 0">
-          <dx-text-box
-            [label]="icon ? '' : label"
-            [value]="value"
-            (onValueChanged)="onChangeValue($event)"
-            [mask]="mask"
-            valueChangeEvent="keyup input change"
-          >
-            <dx-validator [validationRules]="validators" [validationGroup]="validationGroup"></dx-validator>
-            <dxi-button *ngIf="icon"
-                        [options]="{icon, type: 'back', elementAttr: { class: 'field-icon' }}"
-                        name="icon" location="before"
-            ></dxi-button>
-          </dx-text-box>
-        </ng-container>
+      <div class="editor-content-wrapper">
+        <ng-content select="[editor-content]"></ng-content>
+        <dx-text-box *ngIf="!editorContent"
+                     [label]="icon ? '' : label"
+                     [value]="value"
+                     (onValueChanged)="onChangeValue($event)"
+                     [mask]="mask"
+                     valueChangeEvent="keyup input change"
+        >
+          <dx-validator [validationRules]="validators" [validationGroup]="validationGroup"></dx-validator>
+          <dxi-button *ngIf="icon"
+                      [options]="{icon, type: 'back', elementAttr: { class: 'field-icon' }}"
+                      name="icon" location="before"
+          ></dxi-button>
+        </dx-text-box>
       </div>
 
     </ng-template>
@@ -55,35 +55,7 @@ import { CommonModule } from '@angular/common';
       width: 100%;
       height: $field-height;
 
-      .content-ng-wrapper:empty { display: none }
-
-      &::ng-deep .editorContentWrapper {
-        width: 100%;
-
-        .dx-widget.dx-button.field-icon {
-          margin-left: 0;
-          pointer-events: none;
-        }
-      }
-
-      @supports (-moz-appearance:none) {
-        .edit-view-item-value {
-          align-items: center;
-        }
-
-        &::ng-deep .dx-texteditor-input {
-          height: $field-height;
-        }
-      }
-
-      .dx-icon {
-        display: flex;
-        align-items: center;
-        font-size: 18px;
-        margin: 0 $text-button-horizontal-padding;
-      }
-
-      .edit-view-item-wrapper {
+      .view-content-wrapper {
         position: relative;
         padding: 9px 11px 8px;
         block-size: $field-height;
@@ -100,18 +72,48 @@ import { CommonModule } from '@angular/common';
           }
         }
 
-        .edit-view-item-value {
+        .view-content-value {
           display: flex;
           height: calc(#{$field-height} - 17px);
           line-height: 16px;
           font-size: 13px;
         }
       }
+
+      .editor-content-wrapper::ng-deep {
+        width: 100%;
+
+        .dx-widget.dx-button.field-icon {
+          margin-left: 0;
+          pointer-events: none;
+        }
+      }
+
+      @supports (-moz-appearance:none) {
+        .view-content-value {
+          align-items: center;
+        }
+
+        &::ng-deep .dx-texteditor-input {
+          height: $field-height;
+        }
+      }
+
+      .dx-icon {
+        display: flex;
+        align-items: center;
+        font-size: 18px;
+        margin: 0 $text-button-horizontal-padding;
+      }
     }
   `],
 
 })
 export class EditViewItemComponent {
+  @ContentChild(ViewContentDirective) viewContent!: ViewContentDirective;
+
+  @ContentChild(EditorContentDirective) editorContent: EditorContentDirective;
+
   @Input() isEditing: boolean;
 
   @Input() label: string;
@@ -137,7 +139,7 @@ export class EditViewItemComponent {
 
 @NgModule({
   imports: [DxTextBoxModule, CommonModule, DxValidatorModule, DxButtonModule],
-  declarations: [EditViewItemComponent],
-  exports: [EditViewItemComponent],
+  declarations: [EditViewItemComponent, ViewContentDirective, EditorContentDirective],
+  exports: [EditViewItemComponent, ViewContentDirective, EditorContentDirective],
 })
 export class EditViewItemModule { }
