6c60138244e363a4fb62e7eda941ca97ceed949e
groshenkovamarina <groshenkova.marina@devexpress.com>
2022-10-13 19:21:07 +0400

    Try fix tests more

--- packages/testing/tests/crm-contact-list.test.js
+++ packages/testing/tests/crm-contact-list.test.js
@@ -22,14 +22,14 @@ fixture`Contact List`;
       await takeScreenshot(`crm-contact-list-embed=${embedded}-${screenMode[0]}`, 'body');
 
       if (project === 'angular') { // TODO: remove `if` when this react functionality will be ready
-        // if (screenMode[0] === 400) {
-        //   await t.click('.view-wrapper .dx-icon-overflow');
-        // }
-        await t.click(Selector('.dx-button-text').withText('Add contact'));
-        await takeScreenshot(`crm-contact-list-add-contact-form-embed=${embedded}-${screenMode[0]}`, Selector('.data-wrapper'));
         await t.click('tr.dx-data-row:first-child');
         await t.expect(Selector('.contact-name').withText('Amelia Harper').count).eql(1);
         await takeScreenshot(`crm-contact-list-form-embed=${embedded}-${screenMode[0]}`, Selector('.data-wrapper'));
+        if (screenMode[0] === 400) {
+          await t.click('.view-wrapper .dx-icon-overflow');
+        }
+        await t.click(Selector('[aria-label="Add Contact"]'));
+        await takeScreenshot(`crm-contact-list-add-contact-form-embed=${embedded}-${screenMode[0]}`, Selector('.data-wrapper'));
       }
 
       await t
