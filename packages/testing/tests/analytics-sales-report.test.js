2770362f9fc577b726a2869b8af14d32a462be94
MikeVitik <vitikm@gmail.com>
2022-10-24 16:16:50 +0400

    fix screenshort postfix

--- packages/testing/tests/analytics-sales-report.test.js
+++ packages/testing/tests/analytics-sales-report.test.js
@@ -18,17 +18,17 @@ fixture`Analytics Sales Report`;
       await toggleCommonConfiguration(t, BASE_URL, embedded, () => {}, screenMode, timeoutSecond);
 
       await t.expect(Selector('body.dx-device-generic').count).eql(1);
-      await takeScreenshot(`analytics-sales-report-month-embed=${embedded}-${screenMode[0]}`, 'body');
+      await takeScreenshot(`analytics-sales-report-month${getPostfix(embedded, screenMode)}`, 'body');
 
       await t.drag(Selector('.slider').nth(1), -100, 0, { offsetX: 10, offsetY: 10 });
       await t.drag(Selector('.slider').nth(0), 100, 0, { offsetX: 10, offsetY: 10 });
 
-      await takeScreenshot(`analytics-sales-report-month-range-embed=${embedded}-${screenMode[0]}`, 'body');
+      await takeScreenshot(`analytics-sales-report-month-range${getPostfix(embedded, screenMode)}`, 'body');
 
       await t.click(Selector('.dx-dropdownbutton'));
       await t.click(Selector('.dx-dropdownbutton-popup-wrapper .dx-list .dx-list-item').nth(0));
       await t.wait(chartTimeout);
-      await takeScreenshot(`analytics-sales-report-day-range-embed=${embedded}-${screenMode[0]}`, 'body');
+      await takeScreenshot(`analytics-sales-report-day-range${getPostfix(embedded, screenMode)}`, 'body');
 
       await takeScreenshot(`analytics-sales-report-all${getPostfix(embedded, screenMode)}`, 'body');
       if (Selector('.dx-dropdownbutton').length !== 0) {
