3cbe122583af63064a867fbc972aab8d96efc8f6
const314 <constantin.panchenko@gmail.com>
2022-10-25 18:51:02 +0400

    add toolbar buttons, currency format, loading

--- packages/react/src/components/card-analytics/CardAnalytics.tsx
+++ packages/react/src/components/card-analytics/CardAnalytics.tsx
@@ -1,19 +1,35 @@
-import classNames from 'classnames';
 import LoadPanel from 'devextreme-react/load-panel';
-import React, { useState, useEffect } from 'react';
+import Menu from 'devextreme-react/menu';
+import { PositionConfig } from 'devextreme/animation/position';
+import React from 'react';
 import './CardAnalytics.scss';
 
 type CardProps = {
   title?: string;
+  contentClass: string;
+  isLoading?: boolean;
+  kind?: 'normal' | 'small';
 };
 
-export const CardAnalytics = ({ title, children }: React.PropsWithChildren<CardProps>) => {
-  const [loading, setLoading] = useState(true);
+const menuItems: Array<{ icon: string; items: Array<{ text: string }> }> = [
+  {
+    icon: 'overflow',
+    items: [{ text: 'Hide' }],
+  },
+];
+
+export const CardAnalytics = ({ isLoading = false, title, contentClass, children, kind = 'normal' }: React.PropsWithChildren<CardProps>) => {
+  const calculateLoadPanelPosition = (): PositionConfig => ({
+    of: `.${contentClass + (kind === 'normal' ? ' .content' : ' .title')}`,
+    at: kind === 'normal' ? 'center' : 'right',
+  });
 
   return (
-    <div className='card'>
+    <div className={`card ${contentClass}`}>
+      <Menu visible={true} className='overflow-menu' items={menuItems} />
       <div className='title'>{title}</div>
       <div className='content'>{children}</div>
+      <LoadPanel container={`.${contentClass}`} position={calculateLoadPanelPosition()} visible={isLoading} showPane={false} width='100%' height={kind === 'normal' ? 60 : 50} maxWidth='100%' />
     </div>
   );
 };
--- packages/react/src/pages/analytics-dashboard/analytics-dashboard.tsx
+++ packages/react/src/pages/analytics-dashboard/analytics-dashboard.tsx
@@ -1,10 +1,11 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import Toolbar, { Item } from 'devextreme-react/toolbar';
+import Button from 'devextreme-react/button';
 import Bullet, { Size as BulletSize, Tooltip } from 'devextreme-react/bullet';
 import Chart, { Series, CommonSeriesSettings, ArgumentAxis, ValueAxis, Legend, Size, Label, Format, Border } from 'devextreme-react/chart';
 import Funnel, { Label as FunnelLabel, Format as FunnelFormat, Legend as FunnelLegend, Size as FunnelSize, Margin } from 'devextreme-react/funnel';
 import PieChart, { Label as PielLabel, Legend as PieLegend, Size as PieSize, Series as PieSeries, Font, Margin as PieMargin } from 'devextreme-react/pie-chart';
 import DataGrid, { Column } from 'devextreme-react/data-grid';
-import React, { useCallback, useEffect, useState } from 'react';
 import { CardAnalytics } from '../../components/card-analytics/CardAnalytics';
 import { getOpportunitiesByCategory, getSalesByCategory, getSales, getSalesByStateAndCity, getSalesByState } from 'dx-rwa-data';
 import './analytics-dashboard.scss';
@@ -40,6 +41,12 @@ const calculateTotal = (data: Array<any>) => {
   if (!data) return;
   return data.reduce((total, item) => total + (item.value || item.total), 0);
 };
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  maximumFractionDigits: 0,
+});
 
 export const AnalyticsDashboard = () => {
   const [tabIndex, setTabIndex] = useState(tabItems[DEFAULT_TAB_KEY].index);
@@ -97,30 +104,56 @@ export const AnalyticsDashboard = () => {
             onItemClick: onTabClick,
           }}
         />
+        <Item location='after' locateInMenu='auto'>
+          <Button className='add-card' icon='plus' text='Add Card' type='default' stylingMode='contained' />
+        </Item>
+        <Item
+          location='after'
+          locateInMenu='auto'
+          widget='dxButton'
+          showText='inMenu'
+          options={{
+            text: 'Refresh',
+            icon: 'refresh',
+          }}
+        />
+        <Item location='after' locateInMenu='auto'>
+          <div className='separator'></div>
+        </Item>
+        <Item
+          location='after'
+          locateInMenu='auto'
+          widget='dxButton'
+          showText='inMenu'
+          options={{
+            icon: 'export',
+            text: 'Export',
+          }}
+        />
       </Toolbar>
       <div className='cards grey'>
-        <CardAnalytics title='Opportunities'>
-          <div className='total'>${opportunitiesTotal}</div>
+        <CardAnalytics title='Opportunities' contentClass='opportunities-total' kind='small' isLoading={!opportunitiesTotal}>
+          <div className='total'>{currencyFormatter.format(opportunitiesTotal)}</div>
           <div className='percentage'>
             <i className='dx-icon-spinup'></i>
             <span>20.3%</span>
           </div>
         </CardAnalytics>
-        <CardAnalytics title='Revenue Total'>
-          <div className='total'>${salesTotal}</div>
+        <CardAnalytics title='Revenue Total' contentClass='revenue-total' kind='small' isLoading={!salesTotal}>
+          <div className='total'>{currencyFormatter.format(salesTotal)}</div>
           <div className='percentage'>
             <i className='dx-icon-spinup'></i>
             <span>14.7%</span>
           </div>
         </CardAnalytics>
-        <CardAnalytics title='Conversion'>
+        <CardAnalytics title='Conversion' kind='small' contentClass='conversion'>
           <div className='total'>16%</div>
           <div className='percentage'>
             <i className='dx-icon-spindown'></i>
             <span>2.3%</span>
           </div>
         </CardAnalytics>
-        <CardAnalytics title='Leads'>
+        <CardAnalytics contentClass='leads' kind='small' title='Leads'>
           <div className='total'>51</div>
           <div className='percentage'>
             <i className='dx-icon-spinup'></i>
@@ -129,7 +162,7 @@ export const AnalyticsDashboard = () => {
         </CardAnalytics>
       </div>
       <div className='cards'>
-        <CardAnalytics title='Revenue'>
+        <CardAnalytics title='Revenue' contentClass='sales' isLoading={!sales}>
           <Chart dataSource={sales}>
             <Series valueField='total' />
             <CommonSeriesSettings argumentField='date' type='splinearea'>
@@ -145,7 +178,7 @@ export const AnalyticsDashboard = () => {
             <Size height={270} />
           </Chart>
         </CardAnalytics>
-        <CardAnalytics title='Conversion Funnel (All Products)'>
+        <CardAnalytics title='Conversion Funnel (All Products)' contentClass='opportunities' isLoading={!opportunities}>
           <Funnel dataSource={opportunities} argumentField='name' valueField='value'>
             <FunnelLabel visible={true} position='inside' backgroundColor='none' customizeText={({ valueText }) => `$${valueText}`}>
               <FunnelFormat type='largeNumber' precision='1' />
@@ -156,7 +189,7 @@ export const AnalyticsDashboard = () => {
             <FunnelSize height={270} />
           </Funnel>
         </CardAnalytics>
-        <CardAnalytics title='Revenue Analysis'>
+        <CardAnalytics title='Revenue Analysis' contentClass='sales-by-state' isLoading={!salesByState}>
           <DataGrid dataSource={salesByState} height={270}>
             <Column caption='State' dataField='stateName' />
             <Column alignment='left' caption='Sales' dataField='total' dataType='number' format='currency' sortOrder='desc' hidingPriority={2} />
@@ -176,7 +209,7 @@ export const AnalyticsDashboard = () => {
             />
           </DataGrid>
         </CardAnalytics>
-        <CardAnalytics title='Revenue Snapshot (All Products)'>
+        <CardAnalytics title='Revenue Snapshot (All Products)' contentClass='sales-by-category' isLoading={!salesByCategory}>
           <PieChart dataSource={salesByCategory} type='doughnut' diameter={0.8} innerRadius={0.6}>
             <PieSeries argumentField='name' valueField='value'>
               <PielLabel backgroundColor='none' radialOffset={-20} visible={true} customizeText={({ percentText }) => percentText}>
