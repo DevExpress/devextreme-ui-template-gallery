dfbde4ce900ec2c485a4c1ab0104757f85dfc3dd
Shpileva Yuliya <krijovnick@gmail.com>
2022-11-07 09:44:22 +0100

    add toolbar form component

---
+++ packages/react/src/components/toolbar-form/ToolbarForm.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Toolbar, { Item } from 'devextreme-react/toolbar';
+import Button from 'devextreme-react/button';
+
+import './ToolbarForm.scss';
+
+export const ToolbarForm = ({ editing, toggleEditing }) => {
+  return (
+    <Toolbar className='toolbar-form'>
+      <Item location='before'>
+        <span className='dx-form-group-caption'>Details</span>
+      </Item>
+      <Item location='after' locateInMenu='after' visible={!editing}>
+        <Button text='Edit' icon='edit' stylingMode='outlined' type='default' onClick={toggleEditing}></Button>
+      </Item>
+      <Item location='after' locateInMenu='after' visible={editing}>
+        <Button text='Save' stylingMode='outlined' type='default' onClick={toggleEditing}></Button>
+      </Item>
+      <Item location='after' locateInMenu='after' visible={editing}>
+        <Button text='Cancel' stylingMode='text' onClick={toggleEditing}></Button>
+      </Item>
+    </Toolbar>
+  );
+};
