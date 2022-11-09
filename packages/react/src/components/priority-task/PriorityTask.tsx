ca1276ca007692d598c6c316526ee1cc389830c7
Shpileva Yuliya <krijovnick@gmail.com>
2022-11-09 15:00:27 +0100

    fix typo

--- packages/react/src/components/priority-task/PriorityTask.tsx
+++ packages/react/src/components/priority-task/PriorityTask.tsx
@@ -6,6 +6,6 @@ import './PriorityTask.scss';
 export const PriorityTask = ({ text, showText = true }: TaskProp) => (
   <div className={`priority-task priority-${text.toLowerCase()}`}>
     <div className='separator-task'></div>
-    <span>{showText ? text : false}</span>
+    <span>{showText ? text : ''}</span>
   </div>
 );
