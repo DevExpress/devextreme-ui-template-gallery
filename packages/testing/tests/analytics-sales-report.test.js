d7fe628f818d64a306a61c97cb3b212c6d50d716
polosatov.alexander <alexander.polosatov@devexpress.com>
2022-11-10 18:37:49 +0400

    Revert "width check"
    This reverts commit a334ac09bd616808f1e7137a28d5b26295b61e37.

--- packages/testing/tests/analytics-sales-report.test.js
+++ packages/testing/tests/analytics-sales-report.test.js
@@ -1,24 +1,33 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
-import { Selector, ClientFunction } from 'testcafe';
+import { Selector } from 'testcafe';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { getPostfix, toggleCommonConfiguration } from './utils';
 import { screenModes, timeoutSecond } from '../config.js';
 
 const project = process.env.project;
-const BASE_URL = 'http://localhost:4200/#/analytics-sales-report';
+const BASE_URL = `http://localhost:${process.env.port}/#/analytics-sales-report`;
 
 fixture`Analytics Sales Report`;
 
+[false, true].forEach((embedded) => {
+  screenModes.forEach((screenMode) => {
+    test(`Analytics Sales Report (${project}, embed=${embedded}, ${screenMode[0]})`, async (t) => {
+      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+      await toggleCommonConfiguration(t, BASE_URL, embedded, () => {}, screenMode, timeoutSecond);
 
-test('Analytics Sales Report', async (t) => {
-  await toggleCommonConfiguration(t, BASE_URL, false, () => { }, [1280, 800], timeoutSecond);
+      await t.expect(Selector('body.dx-device-generic').count).eql(1);
+      await takeScreenshot(`analytics-sales-report-all${getPostfix(embedded, screenMode)}`, 'body');
+      if (Selector('.dx-dropdownbutton').length !== 0) {
+        await t.click(Selector('.dx-dropdownbutton'));
+        await t.click(Selector('.dx-dropdownbutton-popup-wrapper .dx-list .dx-list-item').nth(0));
+        await t.wait(timeoutSecond);
+        await takeScreenshot(`analytics-sales-report-day${getPostfix(embedded, screenMode)}`, 'body');
+      }
 
-  
-  const getBoxWidth = ClientFunction(() => { 
-    return document.getElementById('range-text').getBBox().width;
+      await t
+        .expect(compareResults.isValid())
+        .ok(compareResults.errorMessages());
+    });
   });
-
-    const width = await getBoxWidth();
-    await t.expect(Math.floor(width)).eql(52);
-  
 });
