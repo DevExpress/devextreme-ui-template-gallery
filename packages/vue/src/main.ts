81ac3dab307c9ca3d1838b78bb7b23f7b5864041
volvl <dxvladislavvolkov@users.noreply.github.com>
2022-11-14 22:23:08 +0400

    Merge styles (#159)
    * Fix indent in templates
    
    * Revert "Fix indent in templates"
    
    This reverts commit 4af66ba85cc26f8d53e2a57df216805eb36c8552.
    
    * Refactoring analytics toolbar
    
    * Revert "Refactoring analytics toolbar"
    
    This reverts commit 5698609d1c1f25a5e791a177c712171b46a3a2ac.
    
    * Merge styles

--- packages/vue/src/main.ts
+++ packages/vue/src/main.ts
@@ -4,7 +4,6 @@ import App from './App.vue';
 import { router } from './router';
 import 'devextreme/scss/bundles/dx.material.blue.light.compact.scss';
 import 'devexpress-gantt/dist/dx-gantt.css';
-import './dx-styles.scss';
 import './styles.scss';
 
 const app = createApp(App);
