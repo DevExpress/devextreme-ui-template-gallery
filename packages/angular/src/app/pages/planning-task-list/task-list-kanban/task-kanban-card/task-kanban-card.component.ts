f46891abc9d4215dbda497ed624cfb7477ce59ec
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-11-14 15:20:39 +0400

    change order

--- packages/angular/src/app/pages/planning-task-list/task-list-kanban/task-kanban-card/task-kanban-card.component.ts
+++ packages/angular/src/app/pages/planning-task-list/task-list-kanban/task-kanban-card/task-kanban-card.component.ts
@@ -22,8 +22,8 @@ export class TaskKanbanCardComponent {
   getAvatarText = (name: string) => name.split(' ').map((name) => name[0]).join('');
 
   notify = (e) => {
-    notify(`Edit '${this.task.text}' card event`);
     e.event.stopPropagation();
+    notify(`Edit '${this.task.text}' card event`);
   };
 
   navigateToDetails = () => {
