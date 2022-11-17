114c34050c0426c6a622f83ee09e2b1293bf4f33
const314 <constantin.panchenko@gmail.com>
2022-11-17 20:07:55 +0400

    wait for fonts for dashboard

--- packages/testing/tests/analytics-dashboard.test.js
+++ packages/testing/tests/analytics-dashboard.test.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
-import { Selector } from 'testcafe';
+import { Selector, RequestLogger } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import { getPostfix, toggleCommonConfiguration } from './utils';
+import {
+  getPostfix, toggleCommonConfiguration, forceResizeRecalculation, awaitFontsLoaded,
+} from './utils';
 import { screenModes, timeoutSecond } from '../config.js';
 
 const project = process.env.project;
 const BASE_URL = `http://localhost:${process.env.port}/#/analytics-dashboard`;
+const requestLogger = RequestLogger();
 
 fixture`Analytics Dashboard`;
 
@@ -21,6 +24,8 @@ const checkScreenMode = async (t, screenMode) => {
       const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
       await toggleCommonConfiguration(t, BASE_URL, embedded, () => {}, screenMode, timeoutSecond);
+      await awaitFontsLoaded(t, requestLogger, 1000);
+      await forceResizeRecalculation(t, screenMode);
 
       await t.expect(Selector('body.dx-device-generic').count).eql(1);
       await takeScreenshot(`analytics-dashboard-all${getPostfix(embedded, screenMode)}`, 'body');
@@ -32,6 +37,6 @@ const checkScreenMode = async (t, screenMode) => {
       await t
         .expect(compareResults.isValid())
         .ok(compareResults.errorMessages());
-    });
+    }).requestHooks(requestLogger);
   });
 });
