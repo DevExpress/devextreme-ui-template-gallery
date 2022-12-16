3ff3e005d6df0b911814586f1b630526dd2268b0
groshenkovamarina <groshenkova.marina@devexpress.com>
2022-12-16 13:54:28 +0400

    Try remove chrome width/height

--- packages/testing/runner.js
+++ packages/testing/runner.js
@@ -27,7 +27,7 @@ createTestCafe('localhost', 1437, 1438)
     testCafe = tc;
 
     const runner = testCafe.createRunner()
-      .browsers(args.mobile ? `chrome:headless:emulation:device=${args.mobile}` : 'chrome:headless:emulation:width=1280;height=800')
+      .browsers(args.mobile ? `chrome:headless:emulation:device=${args.mobile}` : 'chrome:headless')
       .reporter(reporters)
       .src([
         `tests/${args.page}.test.js`,
