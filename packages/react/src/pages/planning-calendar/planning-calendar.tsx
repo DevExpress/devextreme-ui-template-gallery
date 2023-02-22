ad48531b97e448e2b1facc9ee188b535fbae8c56
ivanblinov2k17 <ivanblinov2015@mail.ru>
2023-02-22 17:53:32 +0400

    Basic blocks structure

--- packages/react/src/pages/planning-calendar/planning-calendar.tsx
+++ packages/react/src/pages/planning-calendar/planning-calendar.tsx
@@ -1,8 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
+import Calendar from 'devextreme-react/calendar';
+import Scheduler from 'devextreme-react/scheduler';
+import Button from 'devextreme-react/button';
+import Accordion from 'devextreme-react/accordion';
+
 import './planning-calendar.scss';
 export const PlanningCalendar = () => {
   const [selectedDay] = useState(0);
-  return <>
-    <div>PlaceHolder Today is: {selectedDay} day</div>
-  </>;
+  return <div className='view-wrapper-calendar'>
+    <div className='panels'>
+      <div className='left'>
+        <Button />
+        <Button />
+        <Calendar />
+        <div className='filters-block'>
+          <Accordion />
+        </div>
+      </div>
+      <div className='right'>
+        <Scheduler />
+      </div>
+    </div>
+  </div>;
 };
