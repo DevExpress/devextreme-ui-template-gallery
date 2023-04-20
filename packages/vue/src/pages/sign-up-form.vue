17f32be21d3ba79bebd12782bcc7b4e361f31783
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2023-04-20 23:21:04 +0400

    Add pages

---
+++ packages/vue/src/pages/sign-up-form.vue
@@ -0,0 +1,19 @@
+<template>
+  <CardAuth title="Sign Up">
+    <CreateAccountForm
+      redirectLink="/sign-in-form"
+      buttonLink="/sign-up-form"
+    ></CreateAccountForm>
+  </CardAuth>
+</template>
+
+<script setup lang="ts">
+import CardAuth from '@/components/card-auth.vue';
+import CreateAccountForm from '@/components/create-account-form.vue';
+
+</script>
+
+<style scoped lang="scss">
+@use "@/variables" as *;
+
+</style>
