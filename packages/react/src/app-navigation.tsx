86e0f666606fa442b540bdf982a9f167cee06cbb
ivanblinov2k17 <ivanblinov2015@mail.ru>
2023-05-18 14:04:16 +0400

    fix react navigation

--- packages/react/src/app-navigation.tsx
+++ packages/react/src/app-navigation.tsx
@@ -59,15 +59,15 @@ export const navigation = [
     items: [
       {
         text: 'Sign In Form',
-        path: '/signin-form-page',
+        path: '/sign-in-form',
       },
       {
         text: 'Sign Up Form',
-        path: '/signup-form-page',
+        path: '/sign-up-form',
       },
       {
         text: 'Reset Password Form',
-        path: '/reset-password-form-page',
+        path: '/reset-password-form',
       }
     ]
   },
