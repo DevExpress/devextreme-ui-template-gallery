bd55485df801b514440876702fe2b540a1940ecd
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2023-09-14 15:39:49 +0400

    support fluent theme in setTheme

--- utils/set-theme.js
+++ utils/set-theme.js
@@ -22,11 +22,11 @@ const themeJsFiles = {
 };
 
 const changeThemesMeta = (theme) => {
-  const baseTheme = theme.split('.')[0];
-  const bundleName = theme.replace('generic.', '');
-  const themeParts = bundleName.replace('material.', '').split('.');
-  const mode = themeParts[1];
-  const isDarkTheme = theme.includes('.dark.');
+  const [baseTheme, namePart] = theme.split('.');
+  const isGenericTheme = baseTheme === 'generic';
+  const color = isGenericTheme ? 'blue' : namePart;
+  const isDarkTheme = theme.includes('.dark');
+  const baseBundleName = baseTheme === 'generic' ? '' : `${baseTheme}.${color}.`;
 
   packages.forEach((packageName) => {
     const appPath = join(cwd(), 'packages', packageName);
@@ -38,9 +38,13 @@ const changeThemesMeta = (theme) => {
 
         // main import
         const contentForChange = readFileSync(fileForChange, 'utf8');
-        if (baseTheme === 'generic') {
-          writeFileSync(fileForChange, contentForChange.replace(/material\.blue\./g, ''));
-        }
+
+        writeFileSync(
+          fileForChange,
+          contentForChange
+            .replace(/(scss\/bundles\/dx\.)(.+?\.)*?(dark|light)\.compact(\.scss)?"/g,
+              `$1${baseBundleName}$3.compact$4"`),
+        );
       },
     );
 
@@ -55,15 +59,17 @@ const changeThemesMeta = (theme) => {
       },
     );
 
-    // variables.scss
     const variablesContentForChange = readFileSync(appVariablesPath, 'utf8');
-    let newVariablesContent = variablesContentForChange;
-    if (baseTheme === 'generic') {
-      newVariablesContent = variablesContentForChange.replace('"blue"', '$theme');
-      newVariablesContent = newVariablesContent.replace(', $mode: $theme', '');
-      newVariablesContent = newVariablesContent.replace(/material/g, baseTheme);
+
+    let newVariablesContent = variablesContentForChange.replace(/(material|fluent|generic)/g, baseTheme);
+
+    if (isGenericTheme) {
+      newVariablesContent = newVariablesContent
+        .replace(', $mode: $theme', '')
+        .replace(/\$color: "\w+"/, '$color: $theme');
     } else {
-      newVariablesContent = newVariablesContent.replace('light', mode);
+      newVariablesContent = newVariablesContent
+        .replace('($color: $theme)', `($color: "${color}", $mode: $theme)`);
     }
 
     writeFileSync(appVariablesPath, newVariablesContent);
@@ -74,8 +80,10 @@ const theme = argv[2];
 
 console.log(`Set theme ${theme}`);
 
-if (!theme) {
-  console.log('Usage set-theme.js <themename>');
+if (!/(material|fluent)\.\w+\.(dark|light)\.compact/.test(theme)
+    && !/generic\.(dark|light)\.compact/.test(theme)
+) {
+  console.log('Usage set-theme.js <themename>. Variants: (material|fluent).<color>.(dark|light).compact or generic.(dark|light).compact');
   exit(1);
 }
 
