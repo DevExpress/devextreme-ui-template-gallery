62cc4e7b9716a67f22c83a95d80d19aa8a4fdc58
GoodDayForSurf <GoodDayForSurf@users.noreply.github.com>
2023-09-24 20:02:22 +0000

    Deploying to gh-pages from @ DevExpress/devextreme-ui-template-gallery@879ae02db5cdabb0eae61e90e1132f2df138adfc 🚀

---
+++ generic.dark.compact/react/embedded/static/js/app-theme-dark1.2c410f96.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[334],{24593:function(e,a,p){p.r(a),a.default={}}}]);
+//# sourceMappingURL=app-theme-dark1.2c410f96.chunk.js.map
