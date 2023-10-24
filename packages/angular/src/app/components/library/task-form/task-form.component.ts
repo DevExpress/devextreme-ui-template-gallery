5b20efb665a5513ba90f79cb9b7c22d58a301a7c
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2023-10-24 14:20:50 +0400

    refactor new task onSave on TaskList page for ng

--- packages/angular/src/app/components/library/task-form/task-form.component.ts
+++ packages/angular/src/app/components/library/task-form/task-form.component.ts
@@ -30,7 +30,7 @@ import { ToolbarFormModule } from 'src/app/components/utils/toolbar-form/toolbar
   styleUrls: ['./task-form.component.scss'],
 })
 export class TaskFormComponent implements OnInit {
-  @Input() task: Task;
+  @Input() task!: Task;
 
   @Input() isLoading: boolean = false;
 
@@ -66,6 +66,8 @@ export class TaskFormComponent implements OnInit {
     this.task = { ...this.savedData };
     this.isEditing = false;
   };
+
+  getData = ()=> ({ ...this.task });
 }
 
 @NgModule({
