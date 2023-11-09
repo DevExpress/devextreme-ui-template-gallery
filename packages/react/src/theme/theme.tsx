eb50469695fed3ec31e059af2e13a9adaee6e05d
dxvladslavvolkov <vladislav.volkov@devexpress.com>
2023-11-09 17:25:41 +0400

    Fix charts in fluent

--- packages/react/src/theme/theme.tsx
+++ packages/react/src/theme/theme.tsx
@@ -46,12 +46,14 @@ function switchThemeStyleSheets(enabledTheme: Theme) {
   });
 }
 
-async function setAppTheme(newTheme?: Theme) {
+async function setAppTheme(newTheme?: Theme, isFluent?: boolean) {
   const themeName = newTheme || getCurrentTheme();
 
   switchThemeStyleSheets(themeName);
 
-  currentVizTheme(currentVizTheme().replace(/\.[a-z]+\.compact$/, `.${themeName}.compact`));
+  const regTheme = isFluent ? /\.[a-z]+$/ : /\.[a-z]+\.compact$/;
+  const replaceTheme = isFluent ? `.${themeName}` : `.${themeName}.compact`;
+  currentVizTheme(currentVizTheme().replace(regTheme, replaceTheme));
   refreshTheme();
 }
 
@@ -74,7 +76,7 @@ export function useThemeContext() {
   const switchTheme = useCallback(() => setTheme((currentTheme: Theme) => toggleTeme(currentTheme)), []);
 
   useEffect(() => {
-    isLoaded && setAppTheme(theme);
+    isLoaded && setAppTheme(theme, isFluent());
   }, [theme, isLoaded]);
 
   return useMemo(()=> ({ theme, switchTheme, isLoaded }), [theme, isLoaded]);
