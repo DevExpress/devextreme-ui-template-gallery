fad7ddd35470f02ac5b2a192872b1b7b1329c232
dxvladslavvolkov <vladislav.volkov@devexpress.com>
2023-11-24 10:57:45 +0400

    Add Inter font
    # Conflicts:
    #	packages/angular/src/styles.scss
    #	packages/react/src/styles.scss
    #	packages/vue/src/styles.scss

--- packages/angular/src/app/components/library/app-header/app-header.component.ts
+++ packages/angular/src/app/components/library/app-header/app-header.component.ts
@@ -1,14 +1,22 @@
 import {
-  Component, NgModule, Input, Output, EventEmitter, OnInit,
+  Component,
+  NgModule,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DxButtonModule } from 'devextreme-angular/ui/button';
-import { DxToolbarModule } from 'devextreme-angular/ui/toolbar';
+import { DxToolbarModule, DxToolbarComponent } from 'devextreme-angular/ui/toolbar';
 
 import { UserPanelModule } from '../user-panel/user-panel.component';
 import { AuthService, IUser } from 'src/app/services';
 import { ThemeSwitcherModule } from 'src/app/components/library/theme-switcher/theme-switcher.component';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -16,7 +24,9 @@ import { ThemeSwitcherModule } from 'src/app/components/library/theme-switcher/t
   styleUrls: ['./app-header.component.scss'],
 })
 
-export class AppHeaderComponent implements OnInit {
+export class AppHeaderComponent implements OnInit, AfterViewInit {
+  @ViewChild('toolbar', { static: true }) toolbar!: DxToolbarComponent;
+
   @Output()
   menuToggle = new EventEmitter<boolean>();
 
@@ -37,10 +47,18 @@ export class AppHeaderComponent implements OnInit {
     },
   }];
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private theme: ThemeService) { }
 
   ngOnInit() {
     this.authService.getUser().then((e) => this.user = e.data);
+  }
+
+  ngAfterViewInit(): void {
+    if(this.theme.isFluent()) {
+      setTimeout(() => {
+        this.toolbar.instance.repaint();
+      }, 100);
+    }
   }
 
   toggleMenu = () => {
