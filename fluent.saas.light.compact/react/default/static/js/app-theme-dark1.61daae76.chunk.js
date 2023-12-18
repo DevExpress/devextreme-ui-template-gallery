18dd84d23207742efff06a29f7901e255f3dbefc
GoodDayForSurf <GoodDayForSurf@users.noreply.github.com>
2023-12-18 14:39:12 +0000

    Deploying to gh-pages from @ DevExpress/devextreme-ui-template-gallery@195869e10565b5914dd9a429e252650eb3286905 🚀

---
+++ fluent.saas.light.compact/react/default/static/js/app-theme-dark1.61daae76.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[334],{3459:(e,a,p)=>{p.r(a),p.d(a,{default:()=>c});const c={}}}]);
+//# sourceMappingURL=app-theme-dark1.61daae76.chunk.js.map
