3fbe6fd08c01b6757100ce94e0638eeaaa1637c5
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-03-03 20:43:08 +0200

    Prop 'tone' requires default value to be set (undefined -> eslint)

--- packages/vue/src/components/library/ticker-card.vue
+++ packages/vue/src/components/library/ticker-card.vue
@@ -34,7 +34,7 @@ import { computed, withDefaults } from 'vue';
 const props = withDefaults(
   defineProps<{
     icon: string,
-    tone?: 'warning' | 'info' | undefined,
+    tone?: 'warning' | 'info', // eslint-disable-line vue/require-default-prop
     title: string,
     data?: SalesOrOpportunitiesByCategory | Sales | null,
     total?: string | null,
