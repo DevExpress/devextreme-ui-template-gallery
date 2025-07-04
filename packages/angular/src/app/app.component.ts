cba472ca81ba2065ba982b72a6be067f6c5645c4
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-07-05 01:48:26 +0400

    update angular app for ng20

--- packages/angular/src/app/app.component.ts
+++ packages/angular/src/app/app.component.ts
@@ -1,27 +1,45 @@
-import {Component, HostBinding, inject, OnDestroy,} from '@angular/core';
-import { AppInfoService, AuthService, ScreenService, ThemeService } from './services';
+import { Component, HostBinding, inject, OnDestroy } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+import { DxHttpModule } from "devextreme-angular/http";
+
+import { ScreenService, ThemeService } from './services';
+import { SideNavOuterToolbarModule, SingleCardModule, UnauthenticatedContentModule } from "./layouts";
+import { CrmContactDetailsModule } from "./pages/crm-contact-details/crm-contact-details.component";
+import { PlanningTaskListModule } from "./pages/planning-task-list/planning-task-list.component";
+import { PlanningTaskDetailsModule } from "./pages/planning-task-details/planning-task-details.component";
+import { AnalyticsDashboardModule } from "./pages/analytics-dashboard/analytics-dashboard.component";
+import { AnalyticsSalesReportModule } from "./pages/analytics-sales-report/analytics-sales-report.component";
+import { AnalyticsGeographyModule } from "./pages/analytics-geography/analytics-geography.component";
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
+    imports: [
+      DxHttpModule,
+      SideNavOuterToolbarModule,
+      SingleCardModule,
+      UnauthenticatedContentModule,
+      CrmContactDetailsModule,
+      PlanningTaskListModule,
+      PlanningTaskDetailsModule,
+      AnalyticsDashboardModule,
+      AnalyticsSalesReportModule,
+      AnalyticsGeographyModule,
+      RouterModule,
+  ]
 })
 export class AppComponent implements OnDestroy {
   @HostBinding('class') get getClass() {
     return Object.keys(this.screen.sizes).filter((cl) => this.screen.sizes[cl]).join(' ');
   }
-  private authService = inject(AuthService);
   private themeService =  inject(ThemeService);
   private screen = inject(ScreenService);
-  public appInfo = inject(AppInfoService);
 
   constructor() {
     this.themeService.setAppTheme();
-  }
-
-  isAuthenticated() {
-    return this.authService.loggedIn;
   }
 
   ngOnDestroy(): void {
