0feb87d2b2771d5e206c403e0faad2a765070954
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-07-08 15:12:38 +0300

    fix(ticker-card.vue): missing comma in defineProps obj props (#393)

--- packages/vue/src/components/library/ticker-card.vue
+++ packages/vue/src/components/library/ticker-card.vue
@@ -34,7 +34,7 @@ import { computed, withDefaults } from 'vue';
 const props = withDefaults(
   defineProps<{
     icon: string,
-    tone?: 'warning' | 'info'
+    tone?: 'warning' | 'info',
     title: string,
     data?: SalesOrOpportunitiesByCategory | Sales | null,
     total?: string | null,
