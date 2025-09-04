2aadb26d20f6f118be53f7545b80277c04079b29
Arman Jivanyan <arman.jivanyan@devexpress.com>
2025-09-04 18:18:52 +0400

    revert test change

--- packages/testing/tests/analytics-dashboard.test.js
+++ packages/testing/tests/analytics-dashboard.test.js
@@ -48,7 +48,7 @@ const checkScreenMode = async (t, screenMode) => {
         await t.expect(Selector('body.dx-device-generic').count).eql(1);
         await takeScreenshot(`analytics-dashboard-all${postfix}`, 'body');
         await checkScreenMode(t, screenMode);
-        await t.click(Selector('.dx-tabs .dx-item').nth(3).find('.dx-tab-text-span'));
+        await t.click(Selector('.dx-tabs .dx-item').nth(3).find('.dx-tab-text'));
         await t.wait(1000);
         await takeScreenshot(`analytics-dashboard-year${postfix}`, 'body');
 
