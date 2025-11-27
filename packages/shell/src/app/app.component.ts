0daccaafda9c7a7f60099623a3d20090d2c4f74f
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-11-27 19:01:49 +0100

    update angular in shell

--- packages/shell/src/app/app.component.ts
+++ packages/shell/src/app/app.component.ts
@@ -6,6 +6,7 @@ import allThemes from './themes.json';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: false
 })
 export class AppComponent {
   title = 'shell';
