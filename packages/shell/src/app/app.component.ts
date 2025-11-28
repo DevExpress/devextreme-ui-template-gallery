156baceb894fc9ec49f87daa108f51763f837900
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-11-28 15:21:01 +0100

    update angular in shell fix deps vulnerabilities + fix lint

--- packages/shell/src/app/app.component.ts
+++ packages/shell/src/app/app.component.ts
@@ -6,7 +6,7 @@ import allThemes from './themes.json';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: false
+  standalone: false,
 })
 export class AppComponent {
   title = 'shell';
