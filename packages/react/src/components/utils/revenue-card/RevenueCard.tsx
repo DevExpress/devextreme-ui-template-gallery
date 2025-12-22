c698eef9e2f18a03c35a70c7ff16e7d5b36dc030
Andrei Sergeev <andrey.sergeyev@devexpress.com>
2025-12-22 13:45:53 +0400

    spline gradient (#423)
    * spline gradient
    
    * adjust gradient alpha
    
    * fix etalons
    
    * fix imports

--- packages/react/src/components/utils/revenue-card/RevenueCard.tsx
+++ packages/react/src/components/utils/revenue-card/RevenueCard.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Chart, { ArgumentAxis, Border, CommonSeriesSettings, Format, Label, Legend, Series, Size, ValueAxis } from 'devextreme-react/chart';
+import { ChartsColor, registerGradient } from 'devextreme-react/common/charts';
 import { CardAnalytics } from '../../library/card-analytics/CardAnalytics';
 import { Sale } from '../../../types/analytics';
 
-export const RevenueCard = ({ datasource }: { datasource: Sale[]}) => (
+const seriesColor: ChartsColor = {
+  base: '#115EA3',
+  fillId: registerGradient('linear', {
+    rotationAngle: 90,
+    colors: [{
+      offset: '20%',
+      color: 'rgba(0, 74, 168, 0.75)',
+    }, {
+      offset: '90%',
+      color: 'rgba(114, 179, 231, 0.35)',
+    }],
+  }),
+};
+
+export const RevenueCard = ({ datasource }: { datasource: Sale[] }) => (
   <CardAnalytics title='Revenue' contentClass='sales'>
     <Chart dataSource={datasource}>
       <Series valueField='total' />
-      <CommonSeriesSettings argumentField='date' type='splinearea'>
+      <CommonSeriesSettings argumentField='date' type='splinearea' color={seriesColor}>
         <Border visible />
       </CommonSeriesSettings>
       <ArgumentAxis argumentType='datetime' />
