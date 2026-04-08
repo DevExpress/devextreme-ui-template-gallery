a2aa312525ac11f444a51d3d97b7f003eedcdb55
Arman Jivanyan <arman.jivanyan@devexpress.com>
2026-04-08 12:50:22 +0400

    fix relative paths issue in vite (#455)

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     include: ['dx-template-gallery-data']
