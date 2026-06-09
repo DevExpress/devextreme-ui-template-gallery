b85eb098f44adaf2ab87cbec9f45bd60b91577dc
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-06-09 15:16:49 +0200

    tune analytic-sales test

--- packages/testing/tests/analytics-sales-analysis.test.js
+++ packages/testing/tests/analytics-sales-analysis.test.js
@@ -3,7 +3,7 @@
 import { Selector, RequestLogger } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import {
-  forceResizeRecalculation, getPostfix, toggleCommonConfiguration, setTheme,
+  getPostfix, toggleCommonConfiguration, setTheme,
 } from './utils.js';
 import { screenModes, themeModes, timeoutSecond } from '../config.js';
 
