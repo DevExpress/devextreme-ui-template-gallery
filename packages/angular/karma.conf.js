f3cf9c29e92f981c0093e992eed5be167e7079c3
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-06-09 16:08:00 +0200

    fix review notes

--- packages/angular/karma.conf.js
+++ packages/angular/karma.conf.js
@@ -5,7 +5,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
