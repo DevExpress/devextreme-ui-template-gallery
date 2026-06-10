3d53fa5b12a0eaab5633a27dc0a3096d94373d2b
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-06-10 11:26:17 +0200

    fixes after revert

--- packages/angular/src/app/components/utils/toolbar-analytics/toolbar-analytics.component.ts
+++ packages/angular/src/app/components/utils/toolbar-analytics/toolbar-analytics.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 import { DxButtonComponent } from 'devextreme-angular';
 import { DxTabsModule } from 'devextreme-angular/ui/tabs';
@@ -15,6 +15,7 @@ import { Dates, PanelItem } from 'src/app/types/resource';
   styleUrls: ['./toolbar-analytics.component.scss'],
   imports: [
     AsyncPipe,
+    NgIf,
     DxButtonComponent,
     DxTabsModule,
     DxToolbarModule,
