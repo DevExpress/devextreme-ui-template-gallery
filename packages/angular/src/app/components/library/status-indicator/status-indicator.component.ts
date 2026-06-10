21866631e336d167e705f35c78365d9bb8a7bf93
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-06-10 11:01:58 +0200

    revert moving to @if and @for

--- packages/angular/src/app/components/library/status-indicator/status-indicator.component.ts
+++ packages/angular/src/app/components/library/status-indicator/status-indicator.component.ts
@@ -14,18 +14,15 @@ import { DxTextBoxModule } from 'devextreme-angular/ui/text-box';
       status
       status-indicator
       status-indicator-{{ dashValue }}">
-      @if (!isField) {
-        <span class="status-indicator-{{ dashValue }}">{{ getValue(value) }}</span>
-      }
-      @if (isField) {
-        <dx-text-box
-          class="status-indicator-{{ dashValue }}"
-          [inputAttr]="{class: 'status-input status-editor-input'}"
-          [hoverStateEnabled]="false"
-          [readOnly]="true"
-          [value]="getValue(value)">
-        </dx-text-box>
-      }
+      <span *ngIf="!isField" class="status-indicator-{{ dashValue }}">{{ getValue(value) }}</span>
+      <dx-text-box
+        *ngIf="isField"
+        class="status-indicator-{{ dashValue }}"
+        [inputAttr]="{class: 'status-input status-editor-input'}"
+        [hoverStateEnabled]="false"
+        [readOnly]="true"
+        [value]="getValue(value)">
+      </dx-text-box>
     </div>
   `,
   styleUrls: ['./status-indicator.component.scss'],
